4147ed60a89266c84dec1bc6a76a7e17d7cf42a2
Martin Fond <martin.fond@ensiie.fr>
2023-02-13 14:18:09 +0100

    Ajout prefabs

--- Assets/Scripts/Spells/ProjectileController.cs
+++ Assets/Scripts/Spells/ProjectileController.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class ProjectileController : MonoBehaviour
 {
     public float speed;
+    private float lifeTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        lifeTime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += transform.forward * speed * Time.deltaTime;
+        lifeTime += Time.deltaTime;
+        if (lifeTime > 4)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnCollisionEnter(Collision other)
